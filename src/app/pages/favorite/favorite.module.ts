88e91705974895190aafd121031ad47bfb317e5f
kosimbek kosimov <kosimbekkosimov1999@gmail.com>
2020-09-11 18:16:55 +0500

    создание личного кабинета

--- src/app/pages/favorite/favorite.module.ts
+++ src/app/pages/favorite/favorite.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoriteComponent } from './components/favorite/favorite.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [FavoriteComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class FavoriteModule { }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { PersonalAreaComponent } from './components/personal-area/personal-area.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [LoginComponent, PersonalAreaComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class ProfileModule { }
